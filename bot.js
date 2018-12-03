adba0e4f6c2493583d0a831bcd1a996db71d55ef
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:45:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'lol') {
+    	message.reply('haha');
+  	}
+});
 
 
 client.login(process.env.BOT_TOKEN);
