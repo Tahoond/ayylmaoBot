c2f97e2fa940c1448ac446699a3d7ee775833be0
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 23:01:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'hello') {
+    	message.reply('sup');
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+});
+
+client.on('message', message => {
+    if (message.content === 'code') {
+    	message.reply('find the code on my github, fam!');
+  	}
+});
+
 client.login(process.env.BOT_TOKEN);
