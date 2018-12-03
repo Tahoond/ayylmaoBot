4552ea48d6de927a8b69cb92141a371f181dc478
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:28:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,15 +3,18 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+});
 
+client.on('message', message => {
+    if (message.content === 'hello') {
+    	message.reply('sup');
+  	}
 });
 
 client.on('message', message => {
     if (message.content === '?discord') {
-    	message.reply('add me on discord, theunemans#9256');
+    	message.reply('Add me, theunemans#9256');
   	}
-});
-
 
 
 client.login(process.env.BOT_TOKEN);
