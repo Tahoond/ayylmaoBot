f7c24772d4528474dd80975195fead0107ecd3c4
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:54:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,6 @@ client.on('message', message => {
   	}
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
