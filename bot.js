1705e4c8adb5b90ffa94bd207b33947aaca681a4
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:09:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,6 @@ client.on('message', message => {
   	}
 });
 
-});
-
 client.on('message', message => {
     if (message.content === '?discord') {
     	message.reply('add me on discord, theunemans#9256');
