c9d0ad2eba44407a25779ba47b23a628d714dab8
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:53:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,17 @@ client.on('message', message => {
     	message.reply('sup');
   	}
 });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'code') {
+    	message.reply('find the code on my github, fam!');
+  	}
+});
 
 client.login(process.env.BOT_TOKEN);
