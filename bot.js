70704654381670ed547a6042b3a56d7ed020e8ba
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-03 22:15:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,13 @@ client.on('message', message => {
     	message.reply('add me on discord, theunemans#9256');
   	}
 });
+});
+
+client.on('message', message => {
+    if (message.content === '?ayylmaoBot') {
+    	message.reply('I have no point in my life becouse i am a test bot.');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
