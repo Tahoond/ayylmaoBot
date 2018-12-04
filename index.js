a9314e10a728aa5d05b5f870c07d8812512f641f
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 17:59:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+bot.user.setGame('TEST BOT')
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'hello') {
+    	message.reply('Hi there');
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+});
+
+client.on('message', message => {
+    if (message.content === 'code') {
+    	message.reply('No code for you my man');
+  	}
+});
+
+});
+
+client.on('message', message => {
+    if (message.content === 'fuck') {
+    	message.reply('That is a nono word');
+  	}
+});
+
 client.login(process.env.BOT_TOKEN);
