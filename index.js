1fba7144357f9108fa95ca640b654ea49fcce8cd
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 19:24:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'test') {
-    	message.reply('test');
-  	}
-});
-client.on('message', message => {
-    if (message.content === 'where am i') {
-    	message.reply('space, boi');
-  	}
-});
-client.on('message', message => {
-    if (message.content === 'cool') {
-    	message.reply('freezah');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
