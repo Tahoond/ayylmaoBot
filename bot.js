fcd54107a3de4de451a9dc72df6df5ced48bbfce
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 17:23:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,26 +5,4 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'hello') {
-    	message.reply('Hi there');
-  	}
-});
-
-});
-
-client.on('message', message => {
-    if (message.content === 'code') {
-    	message.reply('No code for you my man');
-  	}
-});
-
-});
-
-client.on('message', message => {
-    if (message.content === 'fuck') {
-    	message.reply('That is a nono word');
-  	}
-});
-
 client.login(process.env.BOT_TOKEN);
