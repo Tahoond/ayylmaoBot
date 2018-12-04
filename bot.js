8c13b8ba76abb2e697dc35a661e232e548b16447
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 22:14:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,12 +11,4 @@ client.on('message', message => {
   	}
 });
 
-});
-
-client.on('message', message => {
-    if (message.content === 'pizza') {
-    	message.reply('My fav food!')
-  	}
-});
-
 client.login(process.env.BOT_TOKEN);
