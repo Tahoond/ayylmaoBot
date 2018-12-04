8c476de203297e28a45882e0e4d1f3c742a3255b
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 17:11:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.user.setGame("i ate too much kfc");
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'hello') {
-    	message.reply('suop mate');
+    	message.reply('Hi there,');
   	}
 });
 
-bot.on*("message, function(message) {
-    if message.author.equals(bot.user)) return;
-
-if (message.content -- "hey") {
-    message.channel.sendMessage("hi");
-
-
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
