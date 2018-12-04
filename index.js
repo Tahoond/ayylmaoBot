e3b349cececc026c2d545225b4e1e7ef31a8498d
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 19:24:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-client.on("ready", () => {
-
-  console.log(`Logged in as ${client.user.username}!`);
-
-  client.user.setActivity('YouTube', { type: "WATCHING" })
-
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-client.on("message", async message => {
-    
-    let prefix = "YOUR BOT PREFIX";
-    
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const input = message.content.slice(prefix.length).split(' ');
-
-    const cmd = input.shift();
-
-if(cmd === `ping`) {
-
-message.channel.send(`Hoold on!`).then(m => {
-
-    m.edit(`🏓  ::  **Pong!** (Roundtrip took: **` + (m.createdTimestamp - message.createdTimestamp) + `ms.** Heartbeat: **` + Math.round(client.ping) + `ms.**)`);
-
-    });
-
- }
-
+client.on('message', message => {
+    if (message.content === 'test') {
+    	message.reply('test');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'where am i') {
+    	message.reply('space, boi');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'cool') {
+    	message.reply('freezah');
+  	}
 });
 
-client.login("NTE4ODk0NDcwMTAwNTQ5Njkx.DuhXjg.VvqZoTayRLVyUhXuTF_AjV0Yn3g");
+client.login(process.env.BOT_TOKEN);
