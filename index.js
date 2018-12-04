bf505b1760fec1d8bcd7f01aa013bfea864d71bb
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-04 19:23:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true});
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", () => {
+
+  console.log(`Logged in as ${client.user.username}!`);
+
+  client.user.setActivity('YouTube', { type: "WATCHING" })
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("message", async message => {
+    
+    let prefix = "YOUR BOT PREFIX";
+    
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const input = message.content.slice(prefix.length).split(' ');
+
+    const cmd = input.shift();
+
+if(cmd === `ping`) {
+
+message.channel.send(`Hoold on!`).then(m => {
+
+    m.edit(`🏓  ::  **Pong!** (Roundtrip took: **` + (m.createdTimestamp - message.createdTimestamp) + `ms.** Heartbeat: **` + Math.round(client.ping) + `ms.**)`);
+
+    });
+
+ }
+
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login("NTE4ODk0NDcwMTAwNTQ5Njkx.DuhXjg.VvqZoTayRLVyUhXuTF_AjV0Yn3g");
