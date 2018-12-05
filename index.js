643f4dfcc5b51e6a502c882c2484bccaa8227bca
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-05 19:10:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-//REQUIRED
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "ayy lmao"
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame(prefix + "ayy lmao");
-    console.log("This bot has booted up and is ready to function!")
 });
 
 client.on('message', message => {
-    if (message.content === 'hello') {
-    	message.reply('Hi there!');
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
-        
-//REQUIRED
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
