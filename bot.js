fc064152b72fb6c04057ade203c3bf7e8e71f34c
Tahoond <38295702+Tahoond@users.noreply.github.com>
2018-12-05 17:49:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,11 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-//cmds
+
 client.on('message', message => {
     if (message.content === 'hello') {
     	message.reply('Hi there!');
   	}
 });
-});
 
-client.on('message', message => {
-    if (message.content === 'pizza') {
-    	message.reply('My favorite food!');
-  	}
-});
-
-//important
 client.login(process.env.BOT_TOKEN);
